c22aeb3bc6212c4b4c95ad5487dfeab23283e005
Newton Bii <biinewton382@gmail.com>
2018-01-08 12:30:12 +0300

    Intitialized database migration.

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, Post
+from app.models import User
 
 # Creating app instance
 app = create_app('development')
